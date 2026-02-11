4f4f7af99494a5968aa0c246416c33468296e282
Igor Santos <igorlimasantos667@gmail.com>
2026-02-11 17:46:19 -0300

    rotas get

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app =  express()
+
+app.get('/projeto' , (req , res)=>{
+    res.json('Rota inicial ')
+    console.log("https://www.google.com")
+})
+app.get('/user' ,(req ,res)=>{
+    res.json('Rota user')
+})
+app.listen(3000 , ()=>{
+    console.log('servidor rodando em http://localhost:3000')
+})
